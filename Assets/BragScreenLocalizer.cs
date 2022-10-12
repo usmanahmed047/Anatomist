298f29d8f1b4f03598480f72810ffaeedd6102e1
Usman Ahmed <usman.ahmed@lemonstudios.io>
2022-10-12 21:45:15 +0500

    Add Assets

---
+++ Assets/BragScreenLocalizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BragScreenLocalizer : MonoBehaviour {
+
+    [SerializeField]
+    Text tryBeatingMe, smartyPants;
+
+    private void OnEnable()
+    {
+        tryBeatingMe.text = LocalizationManager.localization.TryBeatingMe;
+        smartyPants.text = LocalizationManager.localization.SmartyPants;
+
+        tryBeatingMe.GetComponent<ArabicText>().Text = tryBeatingMe.text;
+        tryBeatingMe.GetComponent<ArabicText>().Refresh();
+
+        smartyPants.GetComponent<ArabicText>().Text = smartyPants.text;
+        smartyPants.GetComponent<ArabicText>().Refresh();
+    }
+}
---
+++ Assets/HideIfUnlocked.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideIfUnlocked : MonoBehaviour
+{
+	void Update ()
+    {
+        if (Purchaser.Instance.IsInitialized())
+        {
+            if (Purchaser.Instance.GetPurchasedState(Purchaser.PRODUCT_FULL_APP))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+	}
+}
